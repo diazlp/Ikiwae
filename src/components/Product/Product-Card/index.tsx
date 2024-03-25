d02d02ce9abde0e0a2150ad5160945979c5f2365
Diaz Linggaputra <diazlinggaputra@gmail.com>
2024-03-26 04:24:18 +0700

    refactor: code-splitting to product card

---
+++ src/components/Product/Product-Card/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { FaShoppingCart } from 'react-icons/fa'
+import { Utils } from '../../../lib/utils'
+
+type ProductCardProps = {
+  src: string
+  title: string
+  description: string
+  price: number
+}
+
+const ProductCard: React.FC<ProductCardProps> = ({
+  src,
+  title,
+  description,
+  price
+}) => {
+  return (
+    <div className="col-3 d-flex justify-content-between">
+      <div className="card">
+        <img src={src} alt={title} />
+
+        <div className="card-body">
+          <h5 className="card-title fs-3 fw-bold">{title}</h5>
+          <p className="card-text">{description}</p>
+
+          <div className="d-flex gap-3 align-items-center">
+            <button
+              type="button"
+              className="d-flex align-items-center gap-2 px-2 text-primary-emphasis btn btn-warning"
+            >
+              <FaShoppingCart />
+              Checkout
+            </button>
+            <div className="fw-bold">{Utils.formatCurrency(price)}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProductCard
--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
-import { FaShoppingCart } from 'react-icons/fa'
 import { mejaProduct } from '../../lib/constant'
-import { Utils } from '../../lib/utils'
+import ProductCard from './Product-Card'
 
 const ProductComponent: React.FC = () => {
   return (
@@ -10,27 +9,13 @@ const ProductComponent: React.FC = () => {
 
       <div className="row">
         {mejaProduct.map((meja, index) => (
-          <div className="col-3 d-flex justify-content-between">
-            <div className="card">
-              <img src={meja.src} alt={meja.title} />
-
-              <div className="card-body" key={index}>
-                <h5 className="card-title fs-3 fw-bold">{meja.title}</h5>
-                <p className="card-text">{meja.description}</p>
-
-                <div className="d-flex justify-content-between align-items-center">
-                  <button
-                    type="button"
-                    className="d-flex align-items-center gap-2 px-2 text-primary-emphasis btn btn-warning"
-                  >
-                    <FaShoppingCart />
-                    Checkout
-                  </button>
-                  <div>{Utils.formatCurrency(meja.price)}</div>
-                </div>
-              </div>
-            </div>
-          </div>
+          <ProductCard
+            key={index}
+            src={meja.src}
+            title={meja.title}
+            description={meja.description}
+            price={meja.price}
+          />
         ))}
       </div>
     </section>
