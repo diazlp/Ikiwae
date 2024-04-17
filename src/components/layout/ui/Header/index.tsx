e75dd5f335a294967e34526f8406da6f6e28fce4
Diaz Linggaputra <diazlinggaputra@gmail.com>
2024-04-17 12:10:58 +0700

    feat: invoice page header

--- src/components/layout/ui/Header/index.tsx
+++ src/components/layout/ui/Header/index.tsx
@@ -1,14 +1,18 @@
 import React, { Fragment, useState } from 'react'
-// import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { FaCartShopping } from 'react-icons/fa6'
 import { useCheckoutProduct } from '../../../../contexts/CheckoutContext'
 import CheckoutModal from '../../../CheckoutModal'
 
 import ikiwaeLogo from '/ikiwae-logo.png'
 
-const Header: React.FC = () => {
+type HeaderProps = {
+  isLogoCentered: boolean
+}
+
+const Header: React.FC<HeaderProps> = ({ isLogoCentered }) => {
   const [showCheckoutModal, setShowCheckoutModal] = useState<boolean>(false)
-  // const navigate = useNavigate()
+  const navigate = useNavigate()
   const {
     incrementQty,
     decrementQty,
@@ -24,35 +28,52 @@ const Header: React.FC = () => {
         className="container border-bottom sticky-top"
         style={{ backdropFilter: 'blur(10px)' }}
       >
-        <div className="row" id="header-default">
-          <figure className="col-10 d-flex ">
-            <a href="http://localhost:5173/">
+        {isLogoCentered ? (
+          <div className="row justify-content-center" id="header-default">
+            <figure
+              className="col-10 d-flex justify-content-center "
+              onClick={() => navigate('/')}
+              style={{
+                width: '180px'
+              }}
+            >
               <img
                 src={ikiwaeLogo}
-                className="mt-4 mb-4 w-25 img-fluid"
+                className="mt-4 mb-4 img-fluid"
                 alt="ikiwae logo"
               />
-            </a>
-          </figure>
-
-          <div className="col d-flex align-items-start mt-5">
-            <button
-              type="button"
-              className="btn"
-              // onClick={() => navigate('/invoice')}
-              onClick={() => setShowCheckoutModal(true)}
-            >
-              <div className="row d-flex align-items-center p-3 border gap-2 justify-items-between">
-                <div className="col">
-                  <FaCartShopping color={'#003399'} size={'30px'} />
-                </div>
-                <div className="col fw-bold text-white bg-warning">
-                  {totalQty}
-                </div>
-              </div>
-            </button>
+            </figure>
           </div>
-        </div>
+        ) : (
+          <div className="row" id="header-default">
+            <figure className="col-10 d-flex ">
+              <a href="http://localhost:5173/">
+                <img
+                  src={ikiwaeLogo}
+                  className="mt-4 mb-4 w-25 img-fluid"
+                  alt="ikiwae logo"
+                />
+              </a>
+            </figure>
+
+            <div className="col d-flex align-items-start mt-5">
+              <button
+                type="button"
+                className="btn"
+                onClick={() => setShowCheckoutModal(true)}
+              >
+                <div className="row d-flex align-items-center p-3 border gap-2 justify-items-between">
+                  <div className="col">
+                    <FaCartShopping color={'#003399'} size={'30px'} />
+                  </div>
+                  <div className="col fw-bold text-white bg-warning">
+                    {totalQty}
+                  </div>
+                </div>
+              </button>
+            </div>
+          </div>
+        )}
       </div>
 
       <CheckoutModal
--- src/containers/InvoiceContainer/index.tsx
+++ src/containers/InvoiceContainer/index.tsx
@@ -1,11 +1,11 @@
 import React, { Fragment } from 'react'
 import Header from '../../components/layout/ui/Header'
-import Footer from '../../components/layout/ui/Footer'
+// import Footer from '../../components/layout/ui/Footer'
 
 const InvoiceContainer: React.FC = () => {
   return (
     <Fragment>
-      {/* <Header /> */}
+      <Header isLogoCentered />
       {/* <Footer /> */}
     </Fragment>
   )
